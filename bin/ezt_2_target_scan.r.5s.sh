3aa7856191a493918944bee0c644f4b863ce0d67
tux550 <rodrigosalad@gmail.com>
2022-09-12 13:55:21 -0500

    Refactor

---
+++ bin/ezt_2_target_scan.r.5s.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+if test -f $HOME/.eztarget/eztarget.config; then
+	source $HOME/.eztarget/eztarget.config
+fi
+
+TARGET_STATUS=0 #0=Not set; 1=Found; 2=Out of Reach
+if test -f $TARGET_FILE; then
+	TARGET=$(head -n 1 $TARGET_FILE)
+	if [[ -n "${TARGET// /}" ]]; then
+		if ping -c 1 -t200 $TARGET &> /dev/null; then
+			echo "Target: <span color='$SUCCESS_COLOR'><tt>$TARGET</tt></span>"
+			TARGET_STATUS=1
+		else
+			echo "Target: <span color='$FAIL_COLOR'><tt>$TARGET</tt></span>"
+			TARGET_STATUS=2
+		fi
+	else
+		echo "Target: Not set"
+	fi
+else
+	echo "Target: Not Set"
+fi
+
+echo "---"
+
+if [[ $TARGET_STATUS -eq 1 ]]; then
+	echo "Reports"
+	NMAP_FILE=$SCANS_FOLDER/$TARGET
+	if test -f $NMAP_FILE; then
+		echo "--Ports"
+	fi
+
+	echo "Scan"
+	echo "--Ports | bash='source $INSTALATION_PATH/lib/nmap_target.sh'"
+	echo "--Subdomains "
+	echo "--URL "
+	echo "--(Soon...)"
+	echo "---"
+fi
+echo "Target"
+echo "--Set | bash='source $INSTALATION_PATH/lib/set_target.sh $FILE'"
+#echo "---"
+#echo "Configure (Soon)"
---
+++ lib/nmap_report.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if test -f $HOME/.eztarget/eztarget.config; then
+	source $HOME/.eztarget/eztarget.config
+fi
+
+if test -f $TARGET_FILE; then
+	TARGET=$(head -n 1 $TARGET_FILE)
+	if [[ -n "${TARGET// /}" ]]; then
+		echo "R"
+		echo "---"
+		# REPORT NMAP
+		NMAP_FILE=$SCANS_FOLDER/$TARGET
+		if test -f $NMAP_FILE; then
+			echo "NMAP"
+			# Clean print from grepablefile
+			# Thanks to: https://github.com/leonjza/awesome-nmap-grep
+			egrep -v "^#|Status: Up" $NMAP_FILE | cut -d' ' -f2,4- | \
+			sed -n -e 's/Ignored.*//p'  | \
+			awk '{print "Host: " $1 " Ports: " NF-1; $1=""; for(i=2; i<=NF; i++) { a=a" "$i; }; split(a,s,","); for(e in s) { split(s[e],v,"/"); printf "%-8s %s/%-7s %s\n" , v[2], v[3], v[1], v[5]}; a="" }'
+		fi
+	# Else: Do not Display
+	fi
+# Else: Do not Display
+fi
+

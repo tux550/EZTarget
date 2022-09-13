7440343a2f84e886559676f5c71746c3de2d3537
tux550 <rodrigosalad@gmail.com>
2022-09-13 15:30:43 -0500

    title

--- lib/gobuster_dir_report.sh
+++ lib/gobuster_dir_report.sh
@@ -15,7 +15,7 @@ if [[ -n "${TARGET// /}" ]]; then
 	# REPORT GOBUSTER
 	GOBUSTER_DIR_FILE=$SCANS_FOLDER/gobuster_dir_$TARGET
 	if test -f $GOBUSTER_DIR_FILE; then
-		echo "GOBUSTER"
+		echo "URIs (gobuster)"
 		cat $GOBUSTER_DIR_FILE
 	else
 		echo "Report not found"
